0ab8f29a3964f62d302fead315ff863c00c2e99e
Dani Schufeldt <dschufel@thoughtworks.com>
2015-08-31 15:50:02 +0200

    added real hiccup cures.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def home
-    @hiccup_cure = ["one", "two", "three", "four"].sample
+    @hiccup_cure = ["Take seven smalls sips of something.", "Eat a small piece of dark chocolate.", "Take little sips of water and say mamae after each one. It must be water and it must be ma mae... not mommy or anything else.", "What did you eat for lunch yesterday?", "Pull out your tongue with both hands.","Have someone ask you stupid questions."].sample
   end
 
   def remedy
